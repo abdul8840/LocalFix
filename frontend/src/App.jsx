f10091f622f8b1c90abc4f1e1353ff511533f5ae
abdul8840 <abdul14941naseer@gmail.com>
2026-07-09 16:13:12 +0530

    created home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar.jsx";
+import Footer from "./components/layout/Footer.jsx";
+import Home from "./pages/Home.jsx";
 
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Auth, dashboards, and worker pages come in Step 2+ */}
+          <Route
+            path="*"
+            element={
+              <div className="text-center py-20 text-gray-500">
+                Page not found
+              </div>
+            }
+          />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
-
-export default App
